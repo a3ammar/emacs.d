4104539bdc4887aac2945b2056ce8b0c9499c385
Ammar Alammar <ammar@ammasa.net>
2015-04-12 11:32:10 +0700

    Customization removed from init.el
    Also add cask files to gitignore

--- init.el
+++ init.el
@@ -1,16 +1,2 @@
 (package-initialize)
 (org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" default))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
